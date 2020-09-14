1165ec09835d0e06256303f4043820a1462323c4
Martín Pielvitori <martinpielvitori@gmail.com>
2020-09-13 23:52:44 -0300

    feat: configure tests, db, controller

--- app/tests/controllers/product.test.js
+++ app/tests/controllers/product.test.js
@@ -1,18 +1,45 @@
 /* eslint-disable import/extensions */
-// TODO not working with import -> https://github.com/facebook/jest/issues/9430
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { jest } from '@jest/globals';
+import mongoose from 'mongoose';
+import MongoDBMemory from 'mongodb-memory-server';
 import * as productController from '../../src/controllers/product.js';
 
 describe('Product test', () => {
-  test.skip('simple test', async () => {
+  let mongoServer;
+
+  beforeAll(async () => {
+    mongoServer = new MongoDBMemory.MongoMemoryServer();
+    const mongoUri = await mongoServer.getUri();
+    await mongoose.connect(mongoUri, (err) => {
+      if (err) console.error('Problem connecting DB', err);
+    });
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+  });
+
+  it('get products', async () => {
     const send = jest.fn();
     const res = {
       send,
     };
-    await productController.test({}, res);
+
+    await productController.getProducts({}, res);
+
     expect(send.mock.calls).toHaveLength(1);
-    expect(send.mock.calls[0][0]).toBe('Hello!');
+    expect(send.mock.calls[0][0]).toHaveLength(0);
   });
-  test('fake test', async () => {
-    expect(1).toBe(1);
+
+  it('simple test', async () => {
+    const send = jest.fn();
+    const res = {
+      send,
+    };
+    await productController.testMethod({}, res);
+    expect(send.mock.calls).toHaveLength(1);
+    expect(send.mock.calls[0][0]).toBe('SUCCESS');
   });
 });
